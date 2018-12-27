9fd9cff4995cbf209851d455003aae06bf2ba379
DiK <realcorwin@mail.ru>
2018-12-27 22:43:03 +0100

    shell fix

--- src/main/java/dik/faq/service/StartService.java
+++
@@ -1,6 +0,0 @@
-package dik.faq.service;
-
-public interface StartService {
-
-    public void startExecution();
-}
--- src/main/java/dik/faq/shell/StartCommands.java
+++ src/main/java/dik/faq/shell/StartCommands.java
@@ -1,6 +1,6 @@
 package dik.faq.shell;
 
-import dik.faq.service.StartService;
+import dik.faq.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -8,13 +8,17 @@ import org.springframework.shell.standard.ShellMethod;
 @ShellComponent
 public class StartCommands {
 
+    private TestService testService;
+
     @Autowired
-    StartService startService;
+    public StartCommands(TestService testService) {
+        this.testService = testService;
+    }
 
     @ShellMethod("Start test")
     public void start() {
         // invoke service
-        startService.startExecution();
+        testService.executeTest();
     }
 
 }
